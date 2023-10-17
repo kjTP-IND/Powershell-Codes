0608885ffa9e20a2659c171fadbbdbce84bb8c05
Thadius <thadius@kjcomm.com>
2023-10-17 15:56:41 -0400

    Removed 3 files

--- DefaultApps.ps1
+++
@@ -1,7 +0,0 @@
-SetUserFTA HTTP ChromeHTML;
-SetUserFTA HTTPS ChromeHTML;
-SetUserFTA .HTML ChromeHTML;
-SetUserFTA .HTM ChromeHTML;
-SetUserFTA MAILTO Outlook.URL.mailto.15;
-SetUserFTA .EML Outlook.URL.mailto.15;
-SetUserFTA .PDF AcroExch.Document.DC
