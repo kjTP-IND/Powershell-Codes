235034cb136991b1e04a6d573ea104ca31ab7de0
Thadius <thadius@kjcomm.com>
2023-10-17 15:33:49 -0400

    Creating full file

---
+++ DefaultApps.ps1
@@ -0,0 +1,7 @@
+SetUserFTA HTTP ChromeHTML;
+SetUserFTA HTTPS ChromeHTML;
+SetUserFTA .HTML ChromeHTML;
+SetUserFTA .HTM ChromeHTML;
+SetUserFTA MAILTO Outlook.URL.mailto.15;
+SetUserFTA .EML Outlook.URL.mailto.15;
+SetUserFTA .PDF AcroExch.Document.DC
