07f8c97736dd00c28a6108b53ff4628481b85aa3
Thadius <thadius@kjcomm.com>
2023-10-17 16:10:46 -0400

    changed to exe

--- StartMe.bat
+++ StartMe.bat
@@ -1,2 +1,2 @@
-Powershell -noprofile -ExecutionPolicy Bypass -Command "& {Start-Process Powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""\%~dp0DefaultAndRemoval.ps1\""' -Verb RunAs}"
+Powershell -noprofile -ExecutionPolicy Bypass -Command "& {Start-Process Powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""\%~dp0DefaultsAndRemoval.exe\""' -Verb RunAs}"
 
