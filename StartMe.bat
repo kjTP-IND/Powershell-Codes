47f4162c295d952a3d8c47ac5f866f806bfc786e
Thadius <thadius@kjcomm.com>
2023-10-17 16:23:19 -0400

    trying anything

--- StartMe.bat
+++ StartMe.bat
@@ -1,2 +1,2 @@
-Powershell -noprofile -ExecutionPolicy Bypass -Command "& {Start-Process Powershell -ArgumentList -NoExit '-NoProfile -ExecutionPolicy Bypass -File ""\%~dp0DefaultsAndRemoval.exe\""' -Verb RunAs}"
+Powershell -noprofile -ExecutionPolicy Bypass -Command "& {Start-Process Powershell -ArgumentList -NoExit '-NoProfile -ExecutionPolicy Bypass -File ""& '\%~dp0DefaultsAndRemoval.exe\'""' -Verb RunAs}"
 
