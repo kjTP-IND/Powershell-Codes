c1b023a76beccc50526cd45cd63fb6a586b268ab
Thadius <thadius@kjcomm.com>
2023-10-19 11:58:06 -0400

    Removed need for credentials

--- DefaultsAndRemovals.ps1
+++ DefaultsAndRemovals.ps1
@@ -1,20 +1,14 @@
-$Credentials = Get-Credential
-#AS Local User
-$ProcessJob = Start-Job -Credential $Credentials -ScriptBlock {
-    SetUserFTA HTTP ChromeHTML;
-    SetUserFTA HTTPS ChromeHTML;
-    SetUserFTA .HTML ChromeHTML;
-    SetUserFTA .HTM ChromeHTML;
-    SetUserFTA MAILTO Outlook.URL.mailto.15;
-    SetUserFTA .EML Outlook.URL.mailto.15;
-    SetUserFTA .PDF AcroExch.Document.DC;
-    Get-AppxPackage ClipChamp.Clipchamp | Remove-AppxPackage;
-    Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage;
-    Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage;
-    Get-AppxPackage SpotifyAB.SpotifyMusic | Remove-AppxPackage;
-    Get-AppxPackage Microsoft.GamingApp | Remove-AppxPackage;
-    Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage;
-    Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage;
-};
-Wait-Job $ProcessJob;
-Get-Job;
+SetUserFTA HTTP ChromeHTML;
+SetUserFTA HTTPS ChromeHTML;
+SetUserFTA .HTML ChromeHTML;
+SetUserFTA .HTM ChromeHTML;
+SetUserFTA MAILTO Outlook.URL.mailto.15;
+SetUserFTA .EML Outlook.URL.mailto.15;
+SetUserFTA .PDF AcroExch.Document.DC;
+Get-AppxPackage ClipChamp.Clipchamp | Remove-AppxPackage;
+Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage;
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage;
+Get-AppxPackage SpotifyAB.SpotifyMusic | Remove-AppxPackage;
+Get-AppxPackage Microsoft.GamingApp | Remove-AppxPackage;
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage;
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage;
