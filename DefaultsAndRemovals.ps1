79e7283b029082ed85ceaa0af0d133a628157110
kjTP-IND <144932028+kjTP-IND@users.noreply.github.com>
2023-11-07 12:18:02 -0500

    Added Xbox App to remove

--- DefaultsAndRemovals.ps1
+++ DefaultsAndRemovals.ps1
@@ -11,4 +11,5 @@ Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage;
 Get-AppxPackage SpotifyAB.SpotifyMusic | Remove-AppxPackage;
 Get-AppxPackage Microsoft.GamingApp | Remove-AppxPackage;
 Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage;
+Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage;
 Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage;
