630e566accd2b873d7caa8e7992acff6996208ce
kjTP-IND <144932028+kjTP-IND@users.noreply.github.com>
2023-11-29 11:28:48 -0500

    Update DefaultAndRemovalFullExecution.ps1
    Added McAfee Security removal

--- DefaultAndRemovalFullExecution.ps1
+++ DefaultAndRemovalFullExecution.ps1
@@ -21,6 +21,7 @@ $ProcessJob = Start-Job -Credential $Credentials -ScriptBlock {
     Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage;
     Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage;
     Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage;
+    Get-AppxPackage 5A894077.McAfeeSecurity | Remove-AppxPackage;
 };
 Wait-Job $ProcessJob;
 Get-Job;
